34b9e225b32ccb8ff4ac51ed54837c496d5829d8
Sean Breckenridge <seanbrecke@gmail.com>
2023-09-11 17:49:52 -0700

    quote eval ssh-agent

--- server.sh
+++ server.sh
@@ -4,7 +4,7 @@
 # (structure setup by https://github.com/seanbreckenridge/bootstrap)
 
 if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-	eval $(ssh-agent)
+	eval "$(ssh-agent)"
 	ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
 fi
 export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
